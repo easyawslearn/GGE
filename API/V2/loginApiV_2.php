669b994cf910fe1b434fc151e2556a8a5ca4e00f
AumSahayata <aumsahayata9@gmail.com>
2024-02-16 18:50:26 +0530

    Login API V-2

---
+++ API/V2/loginApiV_2.php
@@ -0,0 +1,41 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require_once('connection.php');
+$response = array();
+
+if ($con) {
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $sql = $con->prepare("SELECT u_id,user_role FROM user WHERE username = BINARY ? AND password = BINARY ? AND is_deleted=false");
+
+        $sql->bind_param('ss', $username, $password);
+        $sql->execute();
+        $result = $sql->get_result();
+        $row = $result->fetch_assoc();
+
+        if ($row > 0) {
+            $response['success'] = true;
+            $response['message'] = 'Login successful';
+            $response['data'] = new stdClass;
+            $response['data']->userId = 3;
+            $response['data']->userRole = "polling_agent";
+
+        } else {
+            $response['success'] = false;
+            $response['message'] = 'Login unsuccessful';
+        }
+    } else {
+        $response['message'] = 'No username or password provided';
+    }
+
+    echo json_encode($response, JSON_PRETTY_PRINT);
+} else {
+    echo "Failed to connect with database";
+}
